c889b7b13328a712d0e4f7fb98963b21a9dcb1ac
Philip Sieder <philip.sieder@gmx.de>
2021-03-07 00:45:44 +0100

    Improve pmap
    * Test whether all the iterable elements are successfully processed. Otherwise throw a `StateError`.
    * Improve code documentation
    * Encapsulate the necessary mutability better
    * Gracefully handly nullable input types and add matching unit tests

--- test/pmap_test.dart
+++ test/pmap_test.dart
@@ -3,8 +3,10 @@ import 'package:test/test.dart';
 
 int _square(int x) => x * x;
 
+int? _maybeSquare(int? x) => x != null ? x * x : null;
+
 void main() {
-  final input = Iterable<int>.generate(1000);
+  final input = Iterable<int>.generate(10);
 
   final mapped = input.map(_square).toList(growable: false);
 
@@ -23,4 +25,16 @@ void main() {
       );
   squareTests(true);
   squareTests(false);
+
+  group('Special cases', () {
+    Iterable.generate(10, (i) => i + 1).forEach(
+        (parallel) => test('Contains null in $parallel isolates', () async {
+              final nullInput =
+                  <int?>[null].followedBy(input).followedBy([null, null]);
+              final pmapped = await (nullInput
+                  .mapParallel(_maybeSquare, parallel: parallel)
+                  .toList());
+              expect(pmapped, equals(nullInput.map(_maybeSquare)));
+            }));
+  });
 }
